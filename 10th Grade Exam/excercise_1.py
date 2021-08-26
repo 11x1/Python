4f4b5c24312f8da238113879d4939846bc4f8d9f
11x1 <89593923+11x1@users.noreply.github.com>
2021-08-26 18:01:10 +0300

    Added excercise_1.py

---
+++ 10th Grade Exam/excercise_1.py
@@ -0,0 +1,15 @@
+# Ülesanne 1
+
+def test(sõne):
+    list(sõne) # -> array
+    final = "" # define final string
+    lastletter = ""
+    # loop through all the letters in list we created earlier
+    for i in range(len(sõne)): # starts from 0 -> last array elem
+        if not sõne[i] == lastletter: # if the letter is not the same as the letter before, add it to the final string variable
+            final += sõne[i]
+        lastletter = sõne[i] # lastletter variable is the current letter to compare it to a letter afterwards
+    print(final)
+    
+test("pprrrrrrrrrrrrrogrammeeriminnnne")
+test("jäääär")
