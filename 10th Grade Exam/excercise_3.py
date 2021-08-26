5098c75814bbdde261b5cc040230835f841eebea
11x1 <89593923+11x1@users.noreply.github.com>
2021-08-26 18:07:30 +0300

    Create excercise_3.py

---
+++ 10th Grade Exam/excercise_3.py
@@ -0,0 +1,10 @@
+# Excercise 3
+#
+# Description
+# While input isn't "Alohomora", ask person "But why?"
+#
+
+while True:
+    input_ = input("What are you doing? ")
+    if input_ == "Alohomora": break # while the input isnt "Alohomora", keep asking "But why?"
+    print("But why?")
