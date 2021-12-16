6135825191903306c3b2e082330e152e3254936a
11x1 <89593923+11x1@users.noreply.github.com>
2021-12-16 12:31:33 -0800

    Update 14.12.2021 4. ülesanne (lisa).py

--- a_klass/14.12.2021 4. ülesanne (lisa).py
+++ a_klass/14.12.2021 4. ülesanne (lisa).py
@@ -1,5 +1,5 @@
 # ♥ ♦ ♣ ♠
-# work in progress, currently only checks for flushes
+# full ghetto but it works hey =)
 
 def kasi(*cards):
     # clamp cards in hand
@@ -30,6 +30,7 @@ def kasi(*cards):
     if all_same_image:
         final = []
         # royal flush
+        print((10 and 11 and 12 and 13 and 14) in card_only_numbers, card_only_numbers)
         if (10 and 11 and 12 and 13 and 14) in card_only_numbers:
             for elem in cards_:
                 final.append(elem.replace('11', 'J').replace('12', 'Q').replace('13', 'K').replace('14', 'A'))
@@ -40,10 +41,9 @@ def kasi(*cards):
         last_card_value = None
         card_only_numbers = sorted(card_only_numbers)
         for card in card_only_numbers:
-            print(card, last_card_value)
             if last_card_value is None:
                 last_card_value = card
-            elif card != last_card_value + 1:
+            elif (card != last_card_value + 1):
                 straight_flush = False
             last_card_value = card
         
@@ -57,7 +57,95 @@ def kasi(*cards):
         final = []
         for elem in cards_:
             final.append(elem.replace('11', 'J').replace('12', 'Q').replace('13', 'K').replace('14', 'A'))
-        print(f"Normal flush: {final}")        
+        print(f"Normal flush: {final}")
 
+    max_count = 0
+    for elem in card_only_numbers:
+        if card_only_numbers.count(elem) > max_count:
+            max_count = card_only_numbers.count(elem)
+    
+    if max_count == 4:
+        final = []
+        for elem in cards_:
+            final.append(elem.replace('11', 'J').replace('12', 'Q').replace('13', 'K').replace('14', 'A'))
+        print(f"Four of a kind: {final}")
+        return
 
-kasi("7♥", "2♥", "4♥", "9♥", "J♥")
+    has_three_same_cards = False
+    has_two_same_cards = False
+    three_card = 0
+
+    for elem in card_only_numbers:
+        if not has_three_same_cards and card_only_numbers.count(elem) == 3:
+            has_three_same_cards = True
+            three_card = elem
+    
+    for elem in card_only_numbers:
+        if not has_two_same_cards and elem != three_card and card_only_numbers.count(elem) == 2:
+            has_two_same_cards = True
+
+    if has_three_same_cards and has_two_same_cards:
+        final = []
+        for elem in cards_:
+            final.append(elem.replace('11', 'J').replace('12', 'Q').replace('13', 'K').replace('14', 'A'))
+        print(f"Full house: {final}")
+        return
+
+    straight = True
+    last_card_value = None
+    card_only_numbers = sorted(card_only_numbers)
+    for card in card_only_numbers:
+            if last_card_value is None:
+                last_card_value = card
+            elif (card != last_card_value + 1):
+                straight = False
+            last_card_value = card
+
+    if straight:
+        final = []
+        for elem in cards_:
+            final.append(elem.replace('11', 'J').replace('12', 'Q').replace('13', 'K').replace('14', 'A'))
+        print(f"Straight: {final}")
+        return
+    
+    if has_three_same_cards:
+        final = []
+        for elem in cards_:
+            final.append(elem.replace('11', 'J').replace('12', 'Q').replace('13', 'K').replace('14', 'A'))
+        print(f"Three of a kind: {final}")
+        return
+
+    has_first_two_pair = False
+    first_two_pair_card = 0
+    has_second_two_pair = False
+    for elem in card_only_numbers:
+        if card_only_numbers.count(elem) == 2:
+            has_first_two_pair = True
+            first_two_pair_card = elem
+    
+    for elem in card_only_numbers:
+        if elem != first_two_pair_card and card_only_numbers.count(elem) == 2:
+            has_second_two_pair = True
+
+    if has_first_two_pair and has_second_two_pair:
+        final = []
+        for elem in cards_:
+            final.append(elem.replace('11', 'J').replace('12', 'Q').replace('13', 'K').replace('14', 'A'))
+        print(f"Two pairs: {final}")
+        return
+    
+    if has_first_two_pair:
+        final = []
+        for elem in cards_:
+            final.append(elem.replace('11', 'J').replace('12', 'Q').replace('13', 'K').replace('14', 'A'))
+        print(f"One pair: {final}")
+        return
+
+    final = []
+    for elem in cards_:
+        final.append(elem.replace('11', 'J').replace('12', 'Q').replace('13', 'K').replace('14', 'A'))
+    print(f"Bad hand: {final}")
+    return
+    
+
+kasi("9♥", "2♥", "J♣", "10♥", "Q♥")
